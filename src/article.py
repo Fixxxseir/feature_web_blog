1027328e2ba45fbc6bc90a7a909f09f4f33d0ebe
mexic <mexic2284@mail.ru>
2024-09-22 21:14:39 +0300

    add search and insert

--- src/article.py
+++ src/article.py
@@ -1,6 +1,45 @@
 class Article:
     """ Класс для хранения статьи """
+    article_id: int
+    title: str
+    content: str
 
-    def __init__(self):
+    articles = dict()  # атрибут на уровне класса для хранения всех статей
+
+    def __init__(self, title: str, content: str):
         """ Конструктор для статьи """
-        pass
+        self.article_id = self.get_new_id()
+        self.title = title
+        self.content = content
+
+    def get_new_id(self) -> int:
+        """ Метод для получения ID следующей статьи"""
+        if len(self.articles) > 0:
+            return max(self.articles.keys()) + 1
+        return 1
+
+    @classmethod
+    def insert(cls, title: str, content: str) -> object:
+        """ Метод для создания и добавления статьи"""
+        new_article = cls(title, content)
+        cls.articles[new_article.article_id] = new_article
+        return new_article
+
+    @classmethod
+    def search(cls, article_id):
+        """ Метод для поиска статьи по ID """
+        return cls.articles[article_id]
+
+
+if __name__ == "__main__":
+    new_article_1 = Article.insert('test1', 'test1')
+    print(new_article_1.article_id)
+
+    new_article_2 = Article.insert('test2', 'test2')
+    print(new_article_2.article_id)
+
+    new_article_3 = Article.insert('test2', 'test2')
+    print(new_article_3.article_id)
+
+    new_article_4 = Article.insert('test2', 'test2')
+    print(new_article_4.article_id)
